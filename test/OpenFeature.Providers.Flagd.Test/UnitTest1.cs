d56e3fd09156dd548c4cb826512e319d28dfd961
Todd Baert <toddbaert@gmail.com>
2022-10-14 12:54:54 -0400

    feat: add scaffolding and publishing (#13)

---
+++ test/OpenFeature.Providers.Flagd.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+
+using Xunit;
+
+namespace OpenFeature.Providers.Flagd.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
