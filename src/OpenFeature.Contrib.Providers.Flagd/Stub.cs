b545d21b19e634ee233bd28f3c6757ade1c6e81e
Todd Baert <toddbaert@gmail.com>
2022-10-15 21:39:49 -0400

    feat!: rename namespace, add OpenFeature dep (#18)
    feat!: rename namespace, add OpenFeature dependency and readmes.

---
+++ src/OpenFeature.Contrib.Providers.Flagd/Stub.cs
@@ -0,0 +1,18 @@
+﻿namespace OpenFeature.Contrib.Providers.Flagd
+{
+    /// <summary>
+    /// A stub class.
+    /// </summary>
+    public class Stub
+    {
+        /// <summary>
+        /// Get the provider name.
+        /// </summary>
+        public static string GetProviderName()
+        {
+            return Api.Instance.GetProviderMetadata().Name;
+        }
+    }
+}
+
+
