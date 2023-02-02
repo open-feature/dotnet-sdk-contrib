617a2f64d60294ea726a819d08e98ce31fdb39db
Eric Holton <eric.holton@gmail.com>
2023-02-02 16:47:18 -0700

    chore: fix white space
    Signed-off-by: Eric Holton <eric.holton@gmail.com>

--- test/OpenFeature.Contrib.Providers.GOFeatureFlag.Test/GoFeatureFlagUserTest.cs
+++ test/OpenFeature.Contrib.Providers.GOFeatureFlag.Test/GoFeatureFlagUserTest.cs
@@ -1,5 +1,7 @@
 using System.Text.Json;
+
 using OpenFeature.Model;
+
 using Xunit;
 
 namespace OpenFeature.Contrib.Providers.GOFeatureFlag.Test;
@@ -10,13 +12,13 @@ public class GoFeatureFlagUserTest
     public void GoFeatureFlagUserSerializesCorrectly()
     {
         var userContext = EvaluationContext.Builder()
-            .Set("targetingKey", "1d1b9238-2591-4a47-94cf-d2bc080892f1") 
+            .Set("targetingKey", "1d1b9238-2591-4a47-94cf-d2bc080892f1")
             .Set("firstname", "john")
             .Set("lastname", "doe")
             .Set("email", "john.doe@gofeatureflag.org")
-            .Set("admin", true) 
+            .Set("admin", true)
             .Set("anonymous", false)
-            .Build();        
+            .Build();
 
         GoFeatureFlagUser user = userContext;
 
