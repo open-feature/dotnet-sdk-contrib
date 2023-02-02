f87923423ff1741695ccadf473076452c134bf2e
Eric Holton <eholton@us.imshealth.com>
2023-02-02 07:25:30 -0700

    fix: GoFeatureFlagUser does not serialize
    System.Text.Json does not serialize private members of a class.
    Since the class GoFeatureFlagUser had the following private
    members:
    ```
            private string Key { get; set; }
            private bool Anonymous { get; set; }
            private Dictionary<string, object> Custom { get; set; }
    ```
    When it was serialized as the User in `GOFeatureFlagRequest`, the Json
    that was returned was `{ "user": {},  ...` instead of
    `{ "user": { "key: :  ...`
    
    This commit fixes this issue by making the property public and
    adding a test.
    
    Signed-off-by: Eric Holton <eric.holton@gmail.com>

---
+++ test/OpenFeature.Contrib.Providers.GOFeatureFlag.Test/GoFeatureFlagUserTest.cs
@@ -0,0 +1,27 @@
+using System.Text.Json;
+using OpenFeature.Model;
+using Xunit;
+
+namespace OpenFeature.Contrib.Providers.GOFeatureFlag.Test;
+
+public class GoFeatureFlagUserTest
+{
+    [Fact]
+    public void GoFeatureFlagUserSerializesCorrectly()
+    {
+        var userContext = EvaluationContext.Builder()
+            .Set("targetingKey", "1d1b9238-2591-4a47-94cf-d2bc080892f1") 
+            .Set("firstname", "john")
+            .Set("lastname", "doe")
+            .Set("email", "john.doe@gofeatureflag.org")
+            .Set("admin", true) 
+            .Set("anonymous", false)
+            .Build();        
+
+        GoFeatureFlagUser user = userContext;
+
+        var userAsString = JsonSerializer.Serialize(user, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+
+        Assert.Contains("{\"key\":\"1d1b9238-2591-4a47-94cf-d2bc080892f1\",\"anonymous\":false,\"custom\":{", userAsString);
+    }
+}
