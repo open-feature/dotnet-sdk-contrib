947b92868512a926a853ab415168b1caf204933f
Vladimir Petrusevici <V.Petrusevici@maib.md>
2023-10-19 10:28:32 +0300

    Add Flagsmith provider
    Signed-off-by: Vladimir Petrusevici <V.Petrusevici@maib.md>
    Signed-off-by: Vladimir Petrusevici <vladimir.petrusevici@outlook.com>

---
+++ src/OpenFeature.Contrib.Providers.Flagsmith/FlagsmithProvider.cs
@@ -0,0 +1,167 @@
+﻿using Flagsmith;
+using OpenFeature.Constant;
+using OpenFeature.Model;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using Trait = Flagsmith.Trait;
+using OpenFeature.Error;
+using System.Globalization;
+
+namespace OpenFeature.Contrib.Providers.Flagsmith
+{
+    /// <summary>
+    /// FlagsmithProvider is the .NET provider implementation for the feature flag solution Flagsmith.
+    /// </summary>
+    public class FlagsmithProvider : FeatureProvider
+    {   
+        private readonly static Metadata Metadata = new("Flagsmith Provider");
+        delegate bool TryParseDelegate<T>(string value, out T x);
+        internal readonly IFlagsmithClient _flagsmithClient;
+
+        /// <summary>
+        /// Key that will be used as identity for Flagsmith requests. Default value is "targetingKey"
+        /// </summary>
+        public string TargetingKey { get; set; } = "targetingKey";
+        /// <summary>
+        /// Creates new instance of <see cref="FlagsmithProvider"/>
+        /// </summary>
+        /// <param name="options">Flagsmith client options. You can just use <see cref="FlagsmithConfiguration"/> class</param>
+        public FlagsmithProvider(IFlagsmithConfiguration options)
+        {
+            _flagsmithClient = new FlagsmithClient(options);
+        }
+
+        /// <summary>
+        /// Creates new instance of <see cref="FlagsmithProvider"/>
+        /// </summary>
+        /// <param name="options">Flagsmith client options. You can just use <see cref="FlagsmithConfiguration"/> class</param>
+        /// <param name="httpClient">Http client that will be used for flagsmith requests. You also can use it to register <see cref="FeatureProvider"/> as Typed HttpClient with <see cref="FeatureProvider"> as abstraction</see></param>
+        public FlagsmithProvider(IFlagsmithConfiguration options, HttpClient httpClient)
+        {
+            _flagsmithClient = new FlagsmithClient(options, httpClient);
+        }
+
+
+        /// <summary>
+        /// Creates new instance of <see cref="FlagsmithProvider"/>
+        /// </summary>
+        /// <param name="flagsmithClient">Precreated Flagsmith client. You can just use <see cref="FlagsmithClient"/> class.</param>
+        public FlagsmithProvider(IFlagsmithClient flagsmithClient)
+        {
+            _flagsmithClient = flagsmithClient;
+        }
+
+        private Task<IFlags> GetFlags(EvaluationContext ctx)
+        {
+            var key = ctx?.GetValue(TargetingKey)?.AsString;
+            return string.IsNullOrEmpty(key)
+                ? _flagsmithClient.GetEnvironmentFlags()
+                : _flagsmithClient.GetIdentityFlags(key, ctx.AsDictionary().Select(x => new Trait(x.Key, x.Value.AsObject) as ITrait).ToList());
+        }
+
+        private async Task<ResolutionDetails<T>> ResolveValue<T>(string flagKey, T defaultValue, TryParseDelegate<T> tryParse, EvaluationContext context)
+        {
+
+            var flags = await GetFlags(context);
+            var isFlagEnabled = await flags.IsFeatureEnabled(flagKey);
+            if (!isFlagEnabled)
+            {
+                return new ResolutionDetails<T>(flagKey, defaultValue, reason: Reason.Disabled);
+            }
+
+            var stringValue = await flags.GetFeatureValue(flagKey);
+
+            if (tryParse(stringValue, out var parsedValue))
+            {
+                return new ResolutionDetails<T>(flagKey, parsedValue);
+            }
+            throw new TypeMismatchException("Failed to parse value in the expected type");
+
+        }
+
+
+        /// <inheritdoc/>
+        public override Metadata GetMetadata() => Metadata;
+
+        /// <inheritdoc/>
+
+        public override Task<ResolutionDetails<bool>> ResolveBooleanValue(string flagKey, bool defaultValue, EvaluationContext context = null)
+            => ResolveValue(flagKey, defaultValue, bool.TryParse, context);
+
+        /// <inheritdoc/>
+        public override Task<ResolutionDetails<int>> ResolveIntegerValue(string flagKey, int defaultValue, EvaluationContext context = null)
+            => ResolveValue(flagKey, defaultValue, int.TryParse, context);
+
+        /// <inheritdoc/>
+        public override Task<ResolutionDetails<double>> ResolveDoubleValue(string flagKey, double defaultValue, EvaluationContext context = null)
+            => ResolveValue(flagKey, defaultValue, (string x, out double y) => double.TryParse(x, NumberStyles.Any, CultureInfo.InvariantCulture, out y), context);
+
+
+        /// <inheritdoc/>
+        public override Task<ResolutionDetails<string>> ResolveStringValue(string flagKey, string defaultValue, EvaluationContext context = null)
+            => ResolveValue(flagKey, defaultValue, (string x, out string y) => { y = x; return true; }, context);
+
+
+        /// <inheritdoc/>
+        public override Task<ResolutionDetails<Value>> ResolveStructureValue(string flagKey, Value defaultValue, EvaluationContext context = null)
+            => ResolveValue(flagKey, defaultValue, TryParseValue, context);
+
+        private bool TryParseValue(string stringValue, out Value result)
+        {
+            try
+            {
+                var mappedValue = JsonNode.Parse(stringValue);
+                result = ConvertValue(mappedValue);
+            }
+            catch
+            {
+                result = null;
+            }
+            return result is not null;
+        }
+
+        /// <summary>
+        ///     convertValue is converting the object return by the proxy response in the right type.
+        /// </summary>
+        /// <param name="node">The value we have received</param>
+        /// <returns>A converted object</returns>
+        private Value ConvertValue(JsonNode node)
+        {
+            if (node == null)
+                return null;
+            if (node is JsonArray jsonArray)
+            {
+                var arr = new List<Value>();
+                foreach (var item in jsonArray)
+                {
+                    var convertedValue = ConvertValue(item);
+                    if (convertedValue != null) arr.Add(convertedValue);
+                }
+                return new Value(arr);
+            }
+
+            if (node is JsonObject jsonObject)
+            {
+                var dict = jsonObject.ToDictionary(x => x.Key, x => ConvertValue(x.Value));
+
+                return new Value(new Structure(dict));
+            }
+
+            if (node.AsValue().TryGetValue<JsonElement>(out var jsonElement))
+            {
+                if (jsonElement.ValueKind == JsonValueKind.False || jsonElement.ValueKind == JsonValueKind.True)
+                    return new Value(jsonElement.GetBoolean());
+                if (jsonElement.ValueKind == JsonValueKind.Number)
+                    return new Value(jsonElement.GetDouble());
+
+                if (jsonElement.ValueKind == JsonValueKind.String)
+                    return new Value(jsonElement.ToString());
+            }
+            return null;
+        }
+    }
+}
---
+++ test/OpenFeature.Contrib.Providers.Flagsmith.Test/FlagsmithProviderTest.cs
@@ -0,0 +1,432 @@
+using Xunit;
+using System;
+using Flagsmith;
+using System.Net.Http;
+using NSubstitute;
+using System.Threading.Tasks;
+using OpenFeature.Constant;
+using OpenFeature.Model;
+using System.Linq;
+using OpenFeature.Error;
+using System.Collections.Generic;
+
+namespace OpenFeature.Contrib.Providers.Flagsmith.Test
+{
+    public class UnitTestFlagsmithProvider
+    {
+        private static FlagsmithConfiguration GetDefaultFlagsmithConfiguration() => new ()
+        {
+            ApiUrl = "https://edge.api.flagsmith.com/api/v1/",
+            EnvironmentKey = string.Empty,
+            EnableClientSideEvaluation = false,
+            EnvironmentRefreshIntervalSeconds = 60,
+            EnableAnalytics = false,
+            Retries = 1
+        };
+
+        [Fact]
+        public void CreateFlagmithProvider_WithValidCredetials_CreatesInstanceSuccessfully()
+        {
+            // Arrange
+            var config = GetDefaultFlagsmithConfiguration();
+
+            // Act
+            var flagsmithProvider = new FlagsmithProvider(config);
+
+
+            // Assert
+            Assert.NotNull(flagsmithProvider._flagsmithClient);
+        }
+
+        [Fact]
+        public void CreateFlagmithProvider_WithValidCredetialsAndCustomHttpClient_CreatesInstanceSuccessfully()
+        {
+            // Arrange
+            var config = GetDefaultFlagsmithConfiguration();
+            using var httpClient = new HttpClient();
+            // Act
+            var flagsmithProvider = new FlagsmithProvider(config, httpClient);
+
+
+            // Assert
+            Assert.NotNull(flagsmithProvider._flagsmithClient);
+        }
+
+        [Fact]
+        public async Task GetValue_ForEnabledFeatureWithEvaluationContext_ReturnCorrectValue()
+        {
+            // Arrange
+            var flagsmithClient = Substitute.For<IFlagsmithClient>();
+            var flags = Substitute.For<IFlags>();
+            var date = DateTime.Now;
+            flags.GetFeatureValue("example-feature").Returns("true");
+            flags.IsFeatureEnabled("example-feature").Returns(true);
+            flagsmithClient.GetIdentityFlags("233", Arg.Is<List<ITrait>>(x => x.Count == 7 && x.Any(c => c.GetTraitKey() == "key1"))).Returns(flags);
+            var flagsmithProvider = new FlagsmithProvider(flagsmithClient);
+
+            var contextBuilder = EvaluationContext.Builder()
+                .Set("key1", "value")
+                .Set("key2", 1)
+                .Set("key3", true)
+                .Set("key4", date)
+                .Set("key5", Structure.Empty)
+                .Set("key6", 1.0)
+                .Set("targetingKey", "233");
+            // Act
+            var result = await flagsmithProvider.ResolveBooleanValue("example-feature", false, contextBuilder.Build());
+
+            // Assert
+            Assert.True(result.Value);
+            Assert.Equal("example-feature", result.FlagKey);
+            Assert.Null(result.Reason);
+            Assert.Equal(ErrorType.None, result.ErrorType);
+        }
+
+        [Fact]
+        public async Task GetBooleanValue_ForEnabledFeatureWithValidFormat_ReturnCorrectValue()
+        {
+            // Arrange
+            var flagsmithClient = Substitute.For<IFlagsmithClient>();
+            var flags = Substitute.For<IFlags>();
+            flags.GetFeatureValue("example-feature").Returns("true");
+            flags.IsFeatureEnabled("example-feature").Returns(true);
+            flagsmithClient.GetEnvironmentFlags().Returns(flags);
+
+            var flagsmithProvider = new FlagsmithProvider(flagsmithClient);
+
+            // Act
+            var result = await flagsmithProvider.ResolveBooleanValue("example-feature", false);
+
+            // Assert
+            Assert.True(result.Value);
+            Assert.Equal("example-feature", result.FlagKey);
+            Assert.Null(result.Reason);
+            Assert.Equal(ErrorType.None, result.ErrorType);
+        }
+
+        [Fact]
+        public async Task GetBooleanValue_ForDisabledFeatureWithValidFormat_ReturnDefaultValue()
+        {
+            // Arrange
+            var flagsmithClient = Substitute.For<IFlagsmithClient>();
+            var flags = Substitute.For<IFlags>();
+            flags.GetFeatureValue("example-feature").Returns("false");
+            flags.IsFeatureEnabled("example-feature").Returns(false);
+            flagsmithClient.GetEnvironmentFlags().Returns(flags);
+
+            var flagsmithProvider = new FlagsmithProvider(flagsmithClient);
+
+            // Act
+            var result = await flagsmithProvider.ResolveBooleanValue("example-feature", true);
+
+            // Assert
+            Assert.True(result.Value);
+            Assert.Equal("example-feature", result.FlagKey);
+            Assert.Equal(Reason.Disabled, result.Reason);
+            Assert.Equal(ErrorType.None, result.ErrorType);
+        }
+
+        [Fact]
+        public async Task GetBooleanValue_ForEnabledFeatureWithWrongFormatValue_ThrowsTypeMismatch()
+        {
+            // Arrange
+            var flagsmithClient = Substitute.For<IFlagsmithClient>();
+            var flags = Substitute.For<IFlags>();
+            flags.GetFeatureValue("example-feature").Returns("hreni");
+            flags.IsFeatureEnabled("example-feature").Returns(true);
+            flagsmithClient.GetEnvironmentFlags().Returns(flags);
+
+            var flagsmithProvider = new FlagsmithProvider(flagsmithClient);
+
+            // Act and Assert
+            await Assert.ThrowsAsync<TypeMismatchException>( () =>  flagsmithProvider.ResolveBooleanValue("example-feature", true));
+
+        }
+
+
+        [Fact]
+        public async Task GetDoubleValue_ForEnabledFeatureWithValidFormat_ReturnCorrectValue()
+        {
+            // Arrange
+            var flagsmithClient = Substitute.For<IFlagsmithClient>();
+            var flags = Substitute.For<IFlags>();
+            flags.GetFeatureValue("example-feature").Returns("32.334");
+            flags.IsFeatureEnabled("example-feature").Returns(true);
+            flagsmithClient.GetEnvironmentFlags().Returns(flags);
+
+            var flagsmithProvider = new FlagsmithProvider(flagsmithClient);
+
+            // Act
+            var result = await flagsmithProvider.ResolveDoubleValue("example-feature", 32.22);
+
+            // Assert
+            Assert.Equal(32.334, result.Value);
+            Assert.Equal("example-feature", result.FlagKey);
+            Assert.Null(result.Reason);
+            Assert.Equal(ErrorType.None, result.ErrorType);
+        }
+
+
+        [Fact]
+        public async Task GetDoubleValue_ForDisabledFeatureWithValidFormat_ReturnDefaultValue()
+        {
+            // Arrange
+            var flagsmithClient = Substitute.For<IFlagsmithClient>();
+            var flags = Substitute.For<IFlags>();
+            flags.GetFeatureValue("example-feature").Returns("4112");
+            flags.IsFeatureEnabled("example-feature").Returns(false);
+            flagsmithClient.GetEnvironmentFlags().Returns(flags);
+
+            var flagsmithProvider = new FlagsmithProvider(flagsmithClient);
+
+            // Act
+            var result = await flagsmithProvider.ResolveDoubleValue("example-feature", -32.22);
+
+            // Assert
+            Assert.Equal(-32.22, result.Value);
+            Assert.Equal("example-feature", result.FlagKey);
+            Assert.Equal(Reason.Disabled, result.Reason);
+            Assert.Equal(ErrorType.None, result.ErrorType);
+        }
+
+        [Fact]
+        public async Task GetDoubleValue_ForEnabledFeatureWithWrongFormatValue_ReturnDefaultValue()
+        {
+            // Arrange
+            var flagsmithClient = Substitute.For<IFlagsmithClient>();
+            var flags = Substitute.For<IFlags>();
+            flags.GetFeatureValue("example-feature").Returns("hreni");
+            flags.IsFeatureEnabled("example-feature").Returns(true);
+            flagsmithClient.GetEnvironmentFlags().Returns(flags);
+
+            var flagsmithProvider = new FlagsmithProvider(flagsmithClient);
+
+            // Act and Assert
+            await Assert.ThrowsAsync<TypeMismatchException>(() => flagsmithProvider.ResolveDoubleValue("example-feature", 2222.22133));
+        }
+
+
+
+        [Fact]
+        public async Task GetStringValue_ForEnabledFeatureWithValidFormat_ReturnCorrectValue()
+        {
+            // Arrange
+            var flagsmithClient = Substitute.For<IFlagsmithClient>();
+            var flags = Substitute.For<IFlags>();
+            flags.GetFeatureValue("example-feature").Returns("example");
+            flags.IsFeatureEnabled("example-feature").Returns(true);
+            flagsmithClient.GetEnvironmentFlags().Returns(flags);
+
+            var flagsmithProvider = new FlagsmithProvider(flagsmithClient);
+
+            // Act
+            var result = await flagsmithProvider.ResolveStringValue("example-feature", "example");
+
+            // Assert
+            Assert.Equal("example", result.Value);
+            Assert.Equal("example-feature", result.FlagKey);
+            Assert.Null(result.Reason);
+            Assert.Equal(ErrorType.None, result.ErrorType);
+        }
+
+
+        [Fact]
+        public async Task GetStringValue_ForDisabledFeatureWithValidFormat_ReturnDefaultValue()
+        {
+            // Arrange
+            var flagsmithClient = Substitute.For<IFlagsmithClient>();
+            var flags = Substitute.For<IFlags>();
+            flags.GetFeatureValue("example-feature").Returns("4112");
+            flags.IsFeatureEnabled("example-feature").Returns(false);
+            flagsmithClient.GetEnvironmentFlags().Returns(flags);
+
+            var flagsmithProvider = new FlagsmithProvider(flagsmithClient);
+
+            // Act
+            var result = await flagsmithProvider.ResolveStringValue("example-feature", "3333a");
+
+            // Assert
+            Assert.Equal("3333a", result.Value);
+            Assert.Equal("example-feature", result.FlagKey);
+            Assert.Equal(Reason.Disabled, result.Reason);
+            Assert.Equal(ErrorType.None, result.ErrorType);
+        }
+
+
+        [Fact]
+        public async Task GetIntValue_ForEnabledFeatureWithValidFormat_ReturnCorrectValue()
+        {
+            // Arrange
+            var flagsmithClient = Substitute.For<IFlagsmithClient>();
+            var flags = Substitute.For<IFlags>();
+            flags.GetFeatureValue("example-feature").Returns("232");
+            flags.IsFeatureEnabled("example-feature").Returns(true);
+            flagsmithClient.GetEnvironmentFlags().Returns(flags);
+
+            var flagsmithProvider = new FlagsmithProvider(flagsmithClient);
+
+            // Act
+            var result = await flagsmithProvider.ResolveIntegerValue("example-feature", 32);
+
+            // Assert
+            Assert.Equal(232, result.Value);
+            Assert.Equal("example-feature", result.FlagKey);
+            Assert.Null(result.Reason);
+            Assert.Equal(ErrorType.None, result.ErrorType);
+        }
+
+        [Fact]
+        public async Task GetIntValue_ForDisabledFeatureWithValidFormat_ReturnDefaultValue()
+        {
+            // Arrange
+            var flagsmithClient = Substitute.For<IFlagsmithClient>();
+            var flags = Substitute.For<IFlags>();
+            flags.GetFeatureValue("example-feature").Returns("4112");
+            flags.IsFeatureEnabled("example-feature").Returns(false);
+            flagsmithClient.GetEnvironmentFlags().Returns(flags);
+
+            var flagsmithProvider = new FlagsmithProvider(flagsmithClient);
+
+            // Act
+            var result = await flagsmithProvider.ResolveIntegerValue("example-feature", -32);
+
+            // Assert
+            Assert.Equal(-32, result.Value);
+            Assert.Equal("example-feature", result.FlagKey);
+            Assert.Equal(Reason.Disabled, result.Reason);
+            Assert.Equal(ErrorType.None, result.ErrorType);
+        }
+
+        [Fact]
+        public async Task GetIntValue_ForEnabledFeatureWithWrongFormatValue_ReturnDefaultValue()
+        {
+            // Arrange
+            var flagsmithClient = Substitute.For<IFlagsmithClient>();
+            var flags = Substitute.For<IFlags>();
+            flags.GetFeatureValue("example-feature").Returns("hreni");
+            flags.IsFeatureEnabled("example-feature").Returns(true);
+            flagsmithClient.GetEnvironmentFlags().Returns(flags);
+
+            var flagsmithProvider = new FlagsmithProvider(flagsmithClient);
+
+            // Act and Assert
+            await Assert.ThrowsAsync<TypeMismatchException>(() => flagsmithProvider.ResolveIntegerValue("example-feature", 2222));
+        }
+
+        [Fact]
+        public async Task GetStructureValue_ForEnabledFeatureWithValidFormat_ReturnCorrectValue()
+        {
+            // Arrange
+            var flagsmithClient = Substitute.For<IFlagsmithClient>();
+            var flags = Substitute.For<IFlags>();
+            var expectedValue =
+                """
+                {
+                  "glossary": {
+                    "title": "example glossary",
+                    "GlossDiv": {
+                      "title": "S",
+                      "GlossList": {
+                        "GlossEntry": {
+                          "ID": "SGML",
+                          "SortAs": "SGML",
+                          "GlossTerm": "Standard Generalized Markup Language",
+                          "Acronym": "SGML",
+                          "Abbrev": "ISO 8879:1986",
+                          "GlossDef": {
+                            "para": "A meta-markup language, used to create markup languages such as DocBook.",
+                            "GlossSeeAlso": [
+                              "GML",
+                              "XML"
+                            ]
+                          },
+                          "GlossSee": "markup"
+                        }
+                      }
+                    }
+                  }
+                }
+                """;
+            flags.GetFeatureValue("example-feature").Returns(expectedValue);
+            flags.IsFeatureEnabled("example-feature").Returns(true);
+            flagsmithClient.GetEnvironmentFlags().Returns(flags);
+
+            var flagsmithProvider = new FlagsmithProvider(flagsmithClient);
+
+            // Act
+            var defaultObject = new Value(Structure.Empty);
+
+            var result = await flagsmithProvider.ResolveStructureValue("example-feature", defaultObject);
+
+            // Assert
+            var glossary = result.Value.AsStructure.GetValue("glossary");
+            Assert.True(glossary.IsStructure);
+            Assert.Equal("example glossary", glossary.AsStructure.GetValue("title").AsString);
+            var glossDiv = glossary.AsStructure.GetValue("GlossDiv");
+            Assert.True(glossDiv.IsStructure);
+            var glossList = glossDiv.AsStructure.GetValue("GlossList");
+            Assert.True(glossList.IsStructure);
+            var glossEntry = glossList.AsStructure.GetValue("GlossEntry");
+            Assert.True(glossEntry.IsStructure);
+            Assert.Equal("SGML", glossEntry.AsStructure.GetValue("SortAs").AsString);
+            var glossDef = glossEntry.AsStructure.GetValue("GlossDef");
+            Assert.True(glossDef.IsStructure);
+            var glossSeeAlso = glossDef.AsStructure.GetValue("GlossSeeAlso");
+            Assert.True(glossSeeAlso.IsList);
+            Assert.Equal(2, glossSeeAlso.AsList.Count);
+            Assert.Equal("GML", glossSeeAlso.AsList.First().AsString);
+            Assert.Equal("XML", glossSeeAlso.AsList.Last().AsString);
+
+            Assert.Equal("example-feature", result.FlagKey);
+            Assert.Null(result.Reason);
+            Assert.Equal(ErrorType.None, result.ErrorType);
+        }
+
+        [Fact]
+        public async Task GetStructureValue_ForDisabledFeatureWithValidFormat_ReturnDefaultValue()
+        {
+            // Arrange
+            var flagsmithClient = Substitute.For<IFlagsmithClient>();
+            var flags = Substitute.For<IFlags>();
+            flags.GetFeatureValue("example-feature").Returns("4112");
+            flags.IsFeatureEnabled("example-feature").Returns(false);
+            flagsmithClient.GetEnvironmentFlags().Returns(flags);
+
+            var defaultObject = new Value("default");
+            var flagsmithProvider = new FlagsmithProvider(flagsmithClient);
+
+            // Act
+            var result = await flagsmithProvider.ResolveStructureValue("example-feature", defaultObject);
+
+            // Assert
+            Assert.Equal(defaultObject, result.Value);
+            Assert.Equal("example-feature", result.FlagKey);
+            Assert.Equal(Reason.Disabled, result.Reason);
+            Assert.Equal(ErrorType.None, result.ErrorType);
+        }
+
+        [Fact]
+        public async Task GetStructureValue_ForEnabledFeatureWithWrongFormatValue_ReturnDefaultValue()
+        {
+            // Arrange
+            var flagsmithClient = Substitute.For<IFlagsmithClient>();
+            var flags = Substitute.For<IFlags>();
+            flags.GetFeatureValue("example-feature").Returns("hreni");
+            flags.IsFeatureEnabled("example-feature").Returns(true);
+            flagsmithClient.GetEnvironmentFlags().Returns(flags);
+
+            var defaultObject = new Value("default");
+            var flagsmithProvider = new FlagsmithProvider(flagsmithClient);
+
+            // Act and Assert
+            await Assert.ThrowsAsync<TypeMismatchException>(() => flagsmithProvider.ResolveStructureValue("example-feature", defaultObject));
+        }
+    }
+
+    public class ExampleConfig
+    {
+        public string ExampleText { get; set; }
+        public int ExampleInt {  get; set; }
+    }
+
+}
