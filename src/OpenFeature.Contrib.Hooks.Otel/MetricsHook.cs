06ba9d42ffbe3fc4621ed035a0e480ed05703df3
André Silva <2493377+askpt@users.noreply.github.com>
2023-12-14 14:58:08 +0000

    Add metrics instrumentation to MetricsHook class
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- src/OpenFeature.Contrib.Hooks.Otel/MetricsHook.cs
+++ src/OpenFeature.Contrib.Hooks.Otel/MetricsHook.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.Metrics;
+using System.Reflection;
 using System.Threading.Tasks;
 using OpenFeature.Model;
 
@@ -9,30 +10,49 @@ namespace OpenFeature.Contrib.Hooks.Otel
 {
     public class MetricsHook : Hook
     {
-        private const string FEATURE_FLAG = "feature_flag";
+        private static readonly AssemblyName AssemblyName = typeof(MetricsHook).Assembly.GetName();
+        private static readonly string InstrumentationName = AssemblyName.Name;
+        private static readonly string InstrumentationVersion = AssemblyName.Version.ToString();
+
         private const string EXCEPTION_ATTR = "exception";
 
-        private const string ACTIVE_COUNT_NAME = FEATURE_FLAG + ".evaluation_active_count";
-        private const string REQUESTS_TOTAL_NAME = FEATURE_FLAG + ".evaluation_requests_total";
-        private const string SUCCESS_TOTAL_NAME = FEATURE_FLAG + ".evaluation_success_total";
-        private const string ERROR_TOTAL_NAME = FEATURE_FLAG + ".evaluation_error_total";
+        private const string ACTIVE_COUNT_NAME = "feature_flag.evaluation_active_count";
+        private const string REQUESTS_TOTAL_NAME = "feature_flag.evaluation_requests_total";
+        private const string SUCCESS_TOTAL_NAME = "feature_flag.evaluation_success_total";
+        private const string ERROR_TOTAL_NAME = "feature_flag.evaluation_error_total";
 
-        private const string KEY_ATTR = FEATURE_FLAG + ".key";
-        private const string PROVIDER_NAME_ATTR = FEATURE_FLAG + ".provider_name";
-        private const string VARIANT_ATTR = FEATURE_FLAG + ".variant";
-        private const string REASON_ATTR = FEATURE_FLAG + ".reason";
+        private const string KEY_ATTR = "feature_flag.key";
+        private const string PROVIDER_NAME_ATTR = "feature_flag.provider_name";
+        private const string VARIANT_ATTR = "feature_flag.variant";
+        private const string REASON_ATTR = "feature_flag.reason";
 
-        // support a functional means of adding custom attributes to metrics, as in the JS implementation, which allows consumers to provide a function that takes the flag metadata 
-        // and returns OTel attributes
+        private readonly UpDownCounter<double> _evaluationActiveUpDownCounter;
+        private readonly Counter<double> _evaluationRequestCounter;
+        private readonly Counter<double> _evaluationSuccessCounter;
+        private readonly Counter<double> _evaluationErrorCounter;
+
         public MetricsHook()
         {
+            var meter = new Meter(InstrumentationName, InstrumentationVersion);
+
+            _evaluationActiveUpDownCounter = meter.CreateUpDownCounter<double>(ACTIVE_COUNT_NAME);
+            _evaluationRequestCounter = meter.CreateCounter<double>(REQUESTS_TOTAL_NAME);
+            _evaluationSuccessCounter = meter.CreateCounter<double>(SUCCESS_TOTAL_NAME);
+            _evaluationErrorCounter = meter.CreateCounter<double>(ERROR_TOTAL_NAME);
         }
 
 
         public override Task<EvaluationContext> Before<T>(HookContext<T> context, IReadOnlyDictionary<string, object> hints = null)
         {
-            // evaluationActiveUpDownCounter
-            // evaluationRequestCounter
+            var tagList = new TagList
+            {
+                { KEY_ATTR, context.FlagKey },
+                { PROVIDER_NAME_ATTR, context.ProviderMetadata.Name }
+            };
+
+            _evaluationActiveUpDownCounter.Add(1, tagList);
+            _evaluationRequestCounter.Add(1, tagList);
+
             return base.Before(context, hints);
         }
 
