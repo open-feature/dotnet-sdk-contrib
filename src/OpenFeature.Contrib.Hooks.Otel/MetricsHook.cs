9d56d1b2125aa7c6f359cc8ff56e225472ae6a6f
André Silva <2493377+askpt@users.noreply.github.com>
2023-12-14 14:42:42 +0000

    Add metrics tracking to MetricsHook class
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- src/OpenFeature.Contrib.Hooks.Otel/MetricsHook.cs
+++ src/OpenFeature.Contrib.Hooks.Otel/MetricsHook.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
 using System.Threading.Tasks;
 using OpenFeature.Model;
 
@@ -7,8 +9,24 @@ namespace OpenFeature.Contrib.Hooks.Otel
 {
     public class MetricsHook : Hook
     {
+        private const string FEATURE_FLAG = "feature_flag";
+        private const string EXCEPTION_ATTR = "exception";
+
+        private const string ACTIVE_COUNT_NAME = FEATURE_FLAG + ".evaluation_active_count";
+        private const string REQUESTS_TOTAL_NAME = FEATURE_FLAG + ".evaluation_requests_total";
+        private const string SUCCESS_TOTAL_NAME = FEATURE_FLAG + ".evaluation_success_total";
+        private const string ERROR_TOTAL_NAME = FEATURE_FLAG + ".evaluation_error_total";
+
+        private const string KEY_ATTR = FEATURE_FLAG + ".key";
+        private const string PROVIDER_NAME_ATTR = FEATURE_FLAG + ".provider_name";
+        private const string VARIANT_ATTR = FEATURE_FLAG + ".variant";
+        private const string REASON_ATTR = FEATURE_FLAG + ".reason";
+
         // support a functional means of adding custom attributes to metrics, as in the JS implementation, which allows consumers to provide a function that takes the flag metadata 
         // and returns OTel attributes
+        public MetricsHook()
+        {
+        }
 
 
         public override Task<EvaluationContext> Before<T>(HookContext<T> context, IReadOnlyDictionary<string, object> hints = null)
