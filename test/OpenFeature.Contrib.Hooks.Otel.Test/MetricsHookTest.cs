64e9e61398ef71a2e9f24bd0c5c76296d30de806
André Silva <2493377+askpt@users.noreply.github.com>
2023-12-14 17:17:21 +0000

    Add Finally_Test to MetricsHookTest.cs
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- test/OpenFeature.Contrib.Hooks.Otel.Test/MetricsHookTest.cs
+++ test/OpenFeature.Contrib.Hooks.Otel.Test/MetricsHookTest.cs
@@ -86,7 +86,40 @@ namespace OpenFeature.Contrib.Hooks.Otel.Test
             Assert.True(noOtherMetric);
         }
         
+        [Fact]
+        public void Finally_Test()
+        {
+            // Arrange metrics collector
+            var exportedItems = new List<Metric>();
+            Sdk.CreateMeterProviderBuilder()
+                .AddMeter("*")
+                .ConfigureResource(r => r.AddService("openfeature"))
+                .AddInMemoryExporter(exportedItems, option => option.PeriodicExportingMetricReaderOptions = new PeriodicExportingMetricReaderOptions { ExportIntervalMilliseconds = 100 })
+                .Build();
+
+            // Arrange
+            const string metricName = "feature_flag.evaluation_active_count";
+            var otelHook = new MetricsHook();
+            var evaluationContext = EvaluationContext.Empty;
+            var ctx = new HookContext<string>("my-flag", "foo", Constant.FlagValueType.String, new ClientMetadata("my-client", "1.0"), new Metadata("my-provider"), evaluationContext);
+
+            // Act
+            var hookTask = otelHook.Finally(ctx, new Dictionary<string, object>());
+            // Wait for the metrics to be exported
+            Thread.Sleep(150);
+
+            // Assert
+            Assert.True(hookTask.IsCompleted);
+            
+            // Assert metrics
+            Assert.NotEmpty(exportedItems);
+            
+            // check if the metric feature_flag.evaluation_success_total is present in the exported items
+            var metric = exportedItems.FirstOrDefault(m => m.Name == metricName);
+            Assert.NotNull(metric);
+            
+            var noOtherMetric = exportedItems.All(m => m.Name == metricName);
+            Assert.True(noOtherMetric);
+        }
     }
-
-
 }
