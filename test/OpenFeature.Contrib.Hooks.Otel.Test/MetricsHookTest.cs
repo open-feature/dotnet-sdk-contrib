fcaea2859d9c4475026f51cc845e5f3849bbe1f1
André Silva <2493377+askpt@users.noreply.github.com>
2023-12-14 17:19:56 +0000

    Refactor metrics hook tests
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- test/OpenFeature.Contrib.Hooks.Otel.Test/MetricsHookTest.cs
+++ test/OpenFeature.Contrib.Hooks.Otel.Test/MetricsHookTest.cs
@@ -1,11 +1,9 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using OpenFeature.Model;
 using OpenTelemetry;
-using OpenTelemetry.Exporter;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 using Xunit;
@@ -42,7 +40,7 @@ namespace OpenFeature.Contrib.Hooks.Otel.Test
             // Assert metrics
             Assert.NotEmpty(exportedItems);
             
-            // check if the metric feature_flag.evaluation_success_total is present in the exported items
+            // check if the metric is present in the exported items
             var metric = exportedItems.FirstOrDefault(m => m.Name == metricName);
             Assert.NotNull(metric);
             
@@ -78,7 +76,7 @@ namespace OpenFeature.Contrib.Hooks.Otel.Test
             // Assert metrics
             Assert.NotEmpty(exportedItems);
             
-            // check if the metric feature_flag.evaluation_success_total is present in the exported items
+            // check if the metric is present in the exported items
             var metric = exportedItems.FirstOrDefault(m => m.Name == metricName);
             Assert.NotNull(metric);
             
@@ -119,6 +117,46 @@ namespace OpenFeature.Contrib.Hooks.Otel.Test
             Assert.NotNull(metric);
             
             var noOtherMetric = exportedItems.All(m => m.Name == metricName);
+            Assert.True(noOtherMetric);
+        }
+
+        [Fact]
+        public void Before_Test()
+        {
+            // Arrange metrics collector
+            var exportedItems = new List<Metric>();
+            Sdk.CreateMeterProviderBuilder()
+                .AddMeter("*")
+                .ConfigureResource(r => r.AddService("openfeature"))
+                .AddInMemoryExporter(exportedItems, option => option.PeriodicExportingMetricReaderOptions = new PeriodicExportingMetricReaderOptions { ExportIntervalMilliseconds = 100 })
+                .Build();
+
+            // Arrange
+            const string metricName1 = "feature_flag.evaluation_active_count";
+            const string metricName2 = "feature_flag.evaluation_requests_total";
+            var otelHook = new MetricsHook();
+            var evaluationContext = EvaluationContext.Empty;
+            var ctx = new HookContext<string>("my-flag", "foo", Constant.FlagValueType.String, new ClientMetadata("my-client", "1.0"), new Metadata("my-provider"), evaluationContext);
+
+            // Act
+            var hookTask = otelHook.Before(ctx, new Dictionary<string, object>());
+            // Wait for the metrics to be exported
+            Thread.Sleep(150);
+
+            // Assert
+            Assert.True(hookTask.IsCompleted);
+            
+            // Assert metrics
+            Assert.NotEmpty(exportedItems);
+            
+            // check if the metric is present in the exported items
+            var metric1 = exportedItems.FirstOrDefault(m => m.Name == metricName1);
+            Assert.NotNull(metric1);
+            
+            var metric2 = exportedItems.FirstOrDefault(m => m.Name == metricName2);
+            Assert.NotNull(metric2);
+            
+            var noOtherMetric = exportedItems.All(m => m.Name == metricName1 || m.Name == metricName2);
             Assert.True(noOtherMetric);
         }
     }
