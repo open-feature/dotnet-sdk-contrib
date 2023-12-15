0a1b1ac76e955df6009d8036fbbd22395450fb97
André Silva <2493377+askpt@users.noreply.github.com>
2023-12-15 13:11:50 +0000

    Renamed test class.
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- test/OpenFeature.Contrib.Hooks.Otel.Test/TracingHookTest.cs
+++ test/OpenFeature.Contrib.Hooks.Otel.Test/TracingHookTest.cs
@@ -10,7 +10,7 @@ using System.Linq;
 
 namespace OpenFeature.Contrib.Hooks.Otel.Test
 {
-    public class OtelHookTest
+    public class TracingHookTest
     {
         [Fact]
         public void TestAfter()
