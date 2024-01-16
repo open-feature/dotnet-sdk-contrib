9759cbddbb51675a21b6a381cddbe51329c814c8
Luiz Bon <292532+luizbon@users.noreply.github.com>
2024-01-17 06:53:17 +1100

    feat: Add ConfigCat provider (#119)
    Signed-off-by: Luiz Bon <luiz.bon@prospa.com>

---
+++ src/OpenFeature.Contrib.Providers.ConfigCat/README.md
@@ -0,0 +1,110 @@
+# ConfigCat Feature Flag .NET Provider
+
+The ConfigCat Flag provider allows you to connect to your ConfigCat instance.
+
+# .Net SDK usage
+
+## Install dependencies
+
+The first things we will do is install the **Open Feature SDK** and the **ConfigCat Feature Flag provider**.
+
+### .NET Cli
+```shell
+dotnet add package OpenFeature.Contrib.Providers.ConfigCat
+```
+### Package Manager
+
+```shell
+NuGet\Install-Package OpenFeature.Contrib.Providers.ConfigCat
+```
+### Package Reference
+
+```xml
+<PackageReference Include="OpenFeature.Contrib.Providers.ConfigCat" />
+```
+### Packet cli
+
+```shell
+paket add OpenFeature.Contrib.Providers.ConfigCat
+```
+
+### Cake
+
+```shell
+// Install OpenFeature.Contrib.Providers.ConfigCat as a Cake Addin
+#addin nuget:?package=OpenFeature.Contrib.Providers.ConfigCat
+
+// Install OpenFeature.Contrib.Providers.ConfigCat as a Cake Tool
+#tool nuget:?package=OpenFeature.Contrib.Providers.ConfigCat
+```
+
+## Using the ConfigCat Provider with the OpenFeature SDK
+
+The following example shows how to use the ConfigCat provider with the OpenFeature SDK.
+
+```csharp
+using OpenFeature.Contrib.Providers.ConfigCat;
+
+namespace OpenFeatureTestApp
+{
+    class Hello {
+        static void Main(string[] args) {
+            var configCatProvider = new ConfigCatProvider("#YOUR-SDK-KEY#");
+
+            // Set the configCatProvider as the provider for the OpenFeature SDK
+            OpenFeature.Api.Instance.SetProvider(configCatProvider);
+
+            var client = OpenFeature.Api.Instance.GetClient();
+
+            var val = client.GetBooleanValue("isMyAwesomeFeatureEnabled", false);
+
+            if(isMyAwesomeFeatureEnabled)
+            {
+                doTheNewThing();
+            }
+            else
+            {
+                doTheOldThing();
+            }
+        }
+    }
+}
+```
+
+### Customizing the ConfigCat Provider
+
+The ConfigCat provider can be customized by passing a `ConfigCatClientOptions` object to the constructor.
+
+```csharp
+var configCatOptions = new ConfigCatClientOptions
+{
+    PollingMode = PollingModes.ManualPoll;
+    Logger = new ConsoleLogger(LogLevel.Info);
+};
+
+var configCatProvider = new ConfigCatProvider("#YOUR-SDK-KEY#", configCatOptions);
+```
+
+For a full list of options see the [ConfigCat documentation](https://configcat.com/docs/sdk-reference/dotnet/).
+
+## EvaluationContext and ConfigCat User relationship
+
+ConfigCat has the concept of Users where you can evaluate a flag based on properties. The OpenFeature SDK has the concept of an EvaluationContext which is a dictionary of string keys and values. The ConfigCat provider will map the EvaluationContext to a ConfigCat User.
+
+The ConfigCat User has a few pre-defined parameters that can be used to evaluate a flag. These are:
+
+| Parameter | Description                                                                                                                     |
+|-----------|---------------------------------------------------------------------------------------------------------------------------------|
+| `Id`      | *REQUIRED*. Unique identifier of a user in your application. Can be any `string` value, even an email address.                  |
+| `Email`   | Optional parameter for easier targeting rule definitions.                                                                       |
+| `Country` | Optional parameter for easier targeting rule definitions.                                                                       |
+| `Custom`  | Optional dictionary for custom attributes of a user for advanced targeting rule definitions. E.g. User role, Subscription type. |
+
+Since EvaluationContext is a simple dictionary, the provider will try to match the keys to the ConfigCat User parameters following the table below in a case-insensitive manner.
+
+| EvaluationContext Key | ConfigCat User Parameter |
+|-----------------------|--------------------------|
+| `id`                  | `Id`                     |
+| `identifier`          | `Id`                     |
+| `email`               | `Email`                  |
+| `country`             | `Country`                |
---
+++ test/OpenFeature.Contrib.Providers.ConfigCat.Test/ConfigCatProviderTest.cs
@@ -0,0 +1,136 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoFixture.Xunit2;
+using ConfigCat.Client;
+using OpenFeature.Constant;
+using OpenFeature.Error;
+using OpenFeature.Model;
+using Xunit;
+
+namespace OpenFeature.Contrib.ConfigCat.Test
+{
+    public class ConfigCatProviderTest
+    {
+        [Theory]
+        [AutoData]
+        public void CreateConfigCatProvider_WithSdkKey_CreatesProviderInstanceSuccessfully(string sdkKey)
+        {
+            var configCatProvider =
+                new ConfigCatProvider(sdkKey, options => { options.FlagOverrides = BuildFlagOverrides(); });
+
+            Assert.NotNull(configCatProvider.Client);
+        }
+
+        [Theory]
+        [InlineAutoData(true, false, true)]
+        [InlineAutoData(false, true, false)]
+        public Task GetBooleanValue_ForFeature_ReturnExpectedResult(object value, bool defaultValue, bool expectedValue, string sdkKey)
+        {
+            return ExecuteResolveTest(value, defaultValue, expectedValue, sdkKey, (provider, key, def) => provider.ResolveBooleanValue(key, def));
+        }
+
+        [Theory]
+        [InlineAutoData("false", true, ErrorType.TypeMismatch)]
+        public Task GetBooleanValue_ForFeature_ShouldThrowException(object value, bool defaultValue, ErrorType expectedErrorType, string sdkKey)
+        {
+            return ExecuteResolveErrorTest(value, defaultValue, expectedErrorType, sdkKey, (provider, key, def) => provider.ResolveBooleanValue(key, def));
+        }
+
+        [Theory]
+        [InlineAutoData(1.0, 2.0, 1.0)]
+        public Task GetDoubleValue_ForFeature_ReturnExpectedResult(object value, double defaultValue, double expectedValue, string sdkKey)
+        {
+            return ExecuteResolveTest(value, defaultValue, expectedValue, sdkKey, (provider, key, def) => provider.ResolveDoubleValue(key, def));
+        }
+
+        [Theory]
+        [InlineAutoData(1, 0, ErrorType.TypeMismatch)]
+        [InlineAutoData("false", 0, ErrorType.TypeMismatch)]
+        [InlineAutoData(false, 0, ErrorType.TypeMismatch)]
+        public Task GetDoubleValue_ForFeature_ShouldThrowException(object value, double defaultValue, ErrorType expectedErrorType, string sdkKey)
+        {
+            return ExecuteResolveErrorTest(value, defaultValue, expectedErrorType, sdkKey, (provider, key, def) => provider.ResolveDoubleValue(key, def));
+        }
+
+        [Theory]
+        [InlineAutoData("some-value", "empty", "some-value")]
+        public Task GetStringValue_ForFeature_ReturnExpectedResult(object value, string defaultValue, string expectedValue, string sdkKey)
+        {
+            return ExecuteResolveTest(value, defaultValue, expectedValue, sdkKey, (provider, key, def) => provider.ResolveStringValue(key, def));
+        }
+
+        [Theory]
+        [InlineAutoData(1, "empty", ErrorType.TypeMismatch)]
+        [InlineAutoData(false, "empty", ErrorType.TypeMismatch)]
+        public Task GetStringValue_ForFeature_ShouldThrowException(object value, string defaultValue, ErrorType expectedErrorType, string sdkKey)
+        {
+            return ExecuteResolveErrorTest(value, defaultValue, expectedErrorType, sdkKey, (provider, key, def) => provider.ResolveStringValue(key, def));
+        }
+
+        [Theory]
+        [InlineAutoData(1, 2, 1)]
+        public Task GetIntValue_ForFeature_ReturnExpectedResult(object value, int defaultValue, int expectedValue, string sdkKey)
+        {
+            return ExecuteResolveTest(value, defaultValue, expectedValue, sdkKey, (provider, key, def) => provider.ResolveIntegerValue(key, def));
+        }
+
+        [Theory]
+        [InlineAutoData(1.0, 0, ErrorType.TypeMismatch)]
+        [InlineAutoData("false", 0, ErrorType.TypeMismatch)]
+        [InlineAutoData(false, 0, ErrorType.TypeMismatch)]
+        public Task GetIntValue_ForFeature_ShouldThrowException(object value, int defaultValue, ErrorType expectedErrorType, string sdkKey)
+        {
+            return ExecuteResolveErrorTest(value, defaultValue, expectedErrorType, sdkKey, (provider, key, def) => provider.ResolveIntegerValue(key, def));
+        }
+
+        [Theory]
+        [AutoData]
+        public async Task GetStructureValue_ForFeature_ReturnExpectedResult(string sdkKey)
+        {
+            const string jsonValue = "{ \"key\": \"value\" }";
+            var defaultValue = new Value(jsonValue);
+            var configCatProvider = new ConfigCatProvider(sdkKey,
+                options => { options.FlagOverrides = BuildFlagOverrides(("example-feature", defaultValue.AsString)); });
+
+            var result = await configCatProvider.ResolveStructureValue("example-feature", defaultValue);
+
+            Assert.Equal(defaultValue.AsString, result.Value.AsString);
+            Assert.Equal("example-feature", result.FlagKey);
+            Assert.Equal(ErrorType.None, result.ErrorType);
+        }
+
+        private static async Task ExecuteResolveTest<T>(object value, T defaultValue, T expectedValue, string sdkKey, Func<ConfigCatProvider, string, T, Task<ResolutionDetails<T>>> resolveFunc)
+        {
+            var configCatProvider = new ConfigCatProvider(sdkKey,
+                options => { options.FlagOverrides = BuildFlagOverrides(("example-feature", value)); });
+
+            var result = await resolveFunc(configCatProvider, "example-feature", defaultValue);
+
+            Assert.Equal(expectedValue, result.Value);
+            Assert.Equal("example-feature", result.FlagKey);
+            Assert.Equal(ErrorType.None, result.ErrorType);
+        }
+
+        private static async Task ExecuteResolveErrorTest<T>(object value, T defaultValue, ErrorType expectedErrorType, string sdkKey, Func<ConfigCatProvider, string, T, Task<ResolutionDetails<T>>> resolveFunc)
+        {
+            var configCatProvider = new ConfigCatProvider(sdkKey,
+                options => { options.FlagOverrides = BuildFlagOverrides(("example-feature", value)); });
+
+            var exception = await Assert.ThrowsAsync<FeatureProviderException>(() => resolveFunc(configCatProvider, "example-feature", defaultValue));
+
+            Assert.Equal(expectedErrorType, exception.ErrorType);
+        }
+
+        private static FlagOverrides BuildFlagOverrides(params (string key, object value)[] values)
+        {
+            var dictionary = new Dictionary<string, object>();
+            foreach (var (key, value) in values)
+            {
+                dictionary.Add(key, value);
+            }
+
+            return FlagOverrides.LocalDictionary(dictionary, OverrideBehaviour.LocalOnly);
+        }
+    }
+}
