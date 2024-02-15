6b0f78016f306ead57798a4e25789c8bf5fec484
Florian Bacher <florian.bacher@dynatrace.com>
2024-02-15 10:06:43 +0100

    add custom jsonLogic string evaluators
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

---
+++ src/OpenFeature.Contrib.Providers.Flagd/Resolver/InProcess/CustomEvaluators/StringEvaluator.cs
@@ -0,0 +1,19 @@
+using System;
+using JsonLogic.Net;
+using Newtonsoft.Json.Linq;
+
+namespace OpenFeature.Contrib.Providers.Flagd.Resolver.InProcess.CustomEvaluators
+{
+    internal class StringEvaluator
+    {
+        internal object StartsWith(IProcessJsonLogic p, JToken[] args, object data)
+        {
+            return p.Apply(args[0], data).ToString().StartsWith(p.Apply(args[1], data).ToString());
+        }
+        
+        internal object EndsWith(IProcessJsonLogic p, JToken[] args, object data)
+        {
+            return p.Apply(args[0], data).ToString().EndsWith(p.Apply(args[1], data).ToString());
+        }
+    }
+}
