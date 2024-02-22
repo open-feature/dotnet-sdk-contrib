5154453a7bf5bf19881dde144f096a5b22065250
Florian Bacher <florian.bacher@dynatrace.com>
2024-02-22 11:30:59 +0100

    fix formatting
    Signed-off-by: Florian Bacher <florian.bacher@dynatrace.com>

--- src/OpenFeature.Contrib.Providers.Flagd/Resolver/InProcess/CustomEvaluators/SemVerEvaluator.cs
+++ src/OpenFeature.Contrib.Providers.Flagd/Resolver/InProcess/CustomEvaluators/SemVerEvaluator.cs
@@ -5,6 +5,7 @@ using Semver;
 
 namespace OpenFeature.Contrib.Providers.Flagd.Resolver.InProcess.CustomEvaluators
 {
+    /// <inheritdoc/>
     public class SemVerEvaluator
     {
         const string OperatorEqual = "=";
@@ -15,7 +16,7 @@ namespace OpenFeature.Contrib.Providers.Flagd.Resolver.InProcess.CustomEvaluator
         const string OperatorGreaterOrEqual = ">=";
         const string OperatorMatchMajor = "^";
         const string OperatorMatchMinor = "~";
-        
+
         internal object Evaluate(IProcessJsonLogic p, JToken[] args, object data)
         {
             // check if we have at least 3 arguments
@@ -25,19 +26,19 @@ namespace OpenFeature.Contrib.Providers.Flagd.Resolver.InProcess.CustomEvaluator
             }
             // get the value from the provided evaluation context
             var versionString = p.Apply(args[0], data).ToString();
-            
+
             // get the operator
             var semVerOperator = p.Apply(args[1], data).ToString();
-            
+
             // get the target version
             var targetVersionString = p.Apply(args[2], data).ToString();
-            
+
             //convert to semantic versions
             try
             {
                 var version = SemVersion.Parse(versionString, SemVersionStyles.Strict);
                 var targetVersion = SemVersion.Parse(targetVersionString, SemVersionStyles.Strict);
-                
+
                 switch (semVerOperator)
                 {
                     case OperatorEqual:
@@ -59,7 +60,8 @@ namespace OpenFeature.Contrib.Providers.Flagd.Resolver.InProcess.CustomEvaluator
                     default:
                         return false;
                 }
-            } catch (Exception e)
+            }
+            catch (Exception)
             {
                 return false;
             }
