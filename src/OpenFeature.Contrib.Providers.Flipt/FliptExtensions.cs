656711bb6db04e6d79abb0e61d2452c30e303b62
Andrei de la Cruz <andrei.delacruz@gmail.com>
2024-09-27 15:56:39 +0200

    Added xmldoc on extension function
    Signed-off-by: Andrei de la Cruz <andrei.delacruz@gmail.com>

--- src/OpenFeature.Contrib.Providers.Flipt/FliptExtensions.cs
+++ src/OpenFeature.Contrib.Providers.Flipt/FliptExtensions.cs
@@ -7,6 +7,11 @@ namespace OpenFeature.Contrib.Providers.Flipt;
 
 public static class FliptExtensions
 {
+    /// <summary>
+    ///     Transforms openFeature EvaluationContext to a mutable Dictionary that flipt sdk accepts
+    /// </summary>
+    /// <param name="evaluationContext">OpenFeature EvaluationContext</param>
+    /// <returns></returns>
     public static Dictionary<string, string> ToStringDictionary(this EvaluationContext evaluationContext)
     {
         return evaluationContext?.AsDictionary().ToDictionary(k => k.Key, v => JsonSerializer.Serialize(v.Value)) ?? [];
