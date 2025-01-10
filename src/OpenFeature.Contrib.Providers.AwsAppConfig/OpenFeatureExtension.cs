668ccf82c76b0c63d0de81c837533a999b5f516c
Ash.Wani <149169001+wani-guanxi@users.noreply.github.com>
2025-01-10 02:16:31 +0000

    Added comments for OpenFeature extension class
    Signed-off-by: Ash.Wani <149169001+wani-guanxi@users.noreply.github.com>

--- src/OpenFeature.Contrib.Providers.AwsAppConfig/OpenFeatureExtension.cs
+++ src/OpenFeature.Contrib.Providers.AwsAppConfig/OpenFeatureExtension.cs
@@ -4,8 +4,18 @@ using Amazon.AppConfigData;
 
 namespace OpenFeature.Contrib.Providers.AwsAppConfig
 {
+    /// <summary>
+    /// Provides extension methods for configuring OpenFeature with AWS AppConfig integration.
+    /// This extension enables feature flag management using AWS AppConfig as the provider.
+    /// </summary>
     public static class OpenFeatureExtension
-    {        
+    {
+        /// <summary>
+        /// Extension method for adding AppConfigProvider to the Serivce Collection.
+        /// </summary>        
+        /// <param name="application">Name of the application for AWS AppConfig</param>
+        /// <param name="environment">Name of the environment for AWS AppConfig</param>
+        /// <returns>The configured OpenFeature API instance.</returns>        
         public static IServiceCollection AddAppConfigProvider(this IServiceCollection services, 
         string application, string environment)
         {
