c30446eb51538b05378db7c4d56228f01ed1cb88
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2025-02-10 11:50:34 +0100

    feat(gofeatureflag): Provider refactor (#313)
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>

---
+++ src/OpenFeature.Contrib.Providers.GOFeatureFlag/models/OfrepRequest.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using System.Text.Json;
+using OpenFeature.Contrib.Providers.GOFeatureFlag.converters;
+using OpenFeature.Contrib.Providers.GOFeatureFlag.exception;
+using OpenFeature.Model;
+
+namespace OpenFeature.Contrib.Providers.GOFeatureFlag
+{
+    /// <summary>
+    ///     GO Feature Flag request to be sent to the evaluation API
+    /// </summary>
+    public class OfrepRequest
+    {
+        private const string KeyField = "targetingKey";
+        private readonly EvaluationContext _ctx;
+
+        /// <summary>
+        ///     Create a new GO Feature Flag request to be sent to the evaluation API
+        /// </summary>
+        /// <param name="ctx"></param>
+        /// <exception cref="InvalidEvaluationContext"></exception>
+        /// <exception cref="InvalidTargetingKey"></exception>
+        public OfrepRequest(EvaluationContext ctx)
+        {
+            try
+            {
+                if (ctx is null)
+                    throw new InvalidEvaluationContext("GO Feature Flag need an Evaluation context to work.");
+                if (!ctx.GetValue(KeyField).IsString)
+                    throw new InvalidTargetingKey("targetingKey field MUST be a string.");
+            }
+            catch (KeyNotFoundException e)
+            {
+                throw new InvalidTargetingKey("targetingKey field is mandatory.", e);
+            }
+
+            _ctx = ctx;
+        }
+
+        /// <summary>
+        ///     Returns the JSON request as string to be sent to the API
+        /// </summary>
+        /// <returns>JSON request as string to be sent to the API</returns>
+        public string AsJsonString()
+        {
+            var request = new Dictionary<string, object> { { "context", _ctx.AsDictionary() } };
+            return JsonSerializer.Serialize(request, JsonConverterExtensions.DefaultSerializerSettings);
+        }
+    }
+}
