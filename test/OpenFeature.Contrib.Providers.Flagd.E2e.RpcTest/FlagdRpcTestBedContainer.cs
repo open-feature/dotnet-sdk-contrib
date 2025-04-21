1d1897c5d7fdfeb5db1726839b10e4b9ff2dd423
Kyle <38759683+kylejuliandev@users.noreply.github.com>
2025-04-21 16:10:58 +0100

    chore: Use TestContainers instead of github services / docker for e2e tests (#345)
    Signed-off-by: Kyle Julian <38759683+kylejuliandev@users.noreply.github.com>
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>
    Co-authored-by: Todd Baert <todd.baert@dynatrace.com>
    Signed-off-by: Weyert de Boer <weyert@innerfuse.biz>

---
+++ test/OpenFeature.Contrib.Providers.Flagd.E2e.RpcTest/FlagdRpcTestBedContainer.cs
@@ -0,0 +1,18 @@
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+
+namespace OpenFeature.Contrib.Providers.Flagd.E2e.RpcTest
+{
+    public class FlagdRpcTestBedContainer
+    {
+        public IContainer Container { get; }
+
+        public FlagdRpcTestBedContainer()
+        {
+            Container = new ContainerBuilder()
+                .WithImage("ghcr.io/open-feature/flagd-testbed:v0.5.21")
+                .WithPortBinding(8013, true)
+                .Build();
+        }
+    }
+}
