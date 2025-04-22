5694f69e24df2db789ef2ee37a3e15b71d93cdb1
Todd Baert <todd.baert@dynatrace.com>
2025-04-22 09:05:06 -0400

    fix: migrate to System.Text.Json and JsonLogic (#347)
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>
    Co-authored-by: André Silva <2493377+askpt@users.noreply.github.com>

--- test/OpenFeature.Contrib.Providers.Flagd.Test/SemVerEvaluatorTest.cs
+++ test/OpenFeature.Contrib.Providers.Flagd.Test/SemVerEvaluatorTest.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
-using JsonLogic.Net;
-using Newtonsoft.Json.Linq;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using Json.Logic;
 using OpenFeature.Contrib.Providers.Flagd.Resolver.InProcess.CustomEvaluators;
 using Xunit;
 
@@ -8,344 +9,323 @@ namespace OpenFeature.Contrib.Providers.Flagd.Test
 {
     public class SemVerEvaluatorTest
     {
-
         [Fact]
         public void EvaluateVersionEqual()
         {
             // Arrange
-            var evaluator = new JsonLogicEvaluator(EvaluateOperators.Default);
-            var semVerEvaluator = new SemVerEvaluator();
-            EvaluateOperators.Default.AddOperator("sem_ver", semVerEvaluator.Evaluate);
+            RuleRegistry.AddRule("sem_ver", new SemVerRule());
 
             var targetingString = @"{""sem_ver"": [
-              {
-                ""var"": [
-                  ""version""
-                ]
-              },
+              { ""var"": ""version"" },
               ""="",
               ""1.0.0""
             ]}";
 
-            // Parse json into hierarchical structure
-            var rule = JObject.Parse(targetingString);
+            var rule = JsonNode.Parse(targetingString);
 
-            var data = new Dictionary<string, string> { { "version", "1.0.0" } };
+            var data = JsonNode.Parse(JsonSerializer.Serialize(new Dictionary<string, string>
+            {
+                { "version", "1.0.0" }
+            }));
 
             // Act & Assert
-            var result = evaluator.Apply(rule, data);
-            Assert.True(result.IsTruthy());
+            var result = JsonLogic.Apply(rule, data);
+            Assert.True(result.GetValue<bool>());
 
-            data.Clear();
-            data.Add("version", "1.0.1");
+            data = JsonNode.Parse(JsonSerializer.Serialize(new Dictionary<string, string>
+            {
+                { "version", "1.0.1" }
+            }));
 
-            result = evaluator.Apply(rule, data);
-            Assert.False(result.IsTruthy());
+            result = JsonLogic.Apply(rule, data);
+            Assert.False(result.GetValue<bool>());
         }
 
         [Fact]
         public void EvaluateVersionNotEqual()
         {
             // Arrange
-            var evaluator = new JsonLogicEvaluator(EvaluateOperators.Default);
-            var semVerEvaluator = new SemVerEvaluator();
-            EvaluateOperators.Default.AddOperator("sem_ver", semVerEvaluator.Evaluate);
+            RuleRegistry.AddRule("sem_ver", new SemVerRule());
 
             var targetingString = @"{""sem_ver"": [
-              {
-                ""var"": [
-                  ""version""
-                ]
-              },
+              { ""var"": ""version"" },
               ""!="",
               ""1.0.0""
             ]}";
 
-            // Parse json into hierarchical structure
-            var rule = JObject.Parse(targetingString);
+            var rule = JsonNode.Parse(targetingString);
 
-            var data = new Dictionary<string, string> { { "version", "1.0.0" } };
+            var data = JsonNode.Parse(JsonSerializer.Serialize(new Dictionary<string, string>
+            {
+                { "version", "1.0.0" }
+            }));
 
             // Act & Assert
-            var result = evaluator.Apply(rule, data);
-            Assert.False(result.IsTruthy());
+            var result = JsonLogic.Apply(rule, data);
+            Assert.False(result.GetValue<bool>());
 
-            data.Clear();
-            data.Add("version", "1.0.1");
+            data = JsonNode.Parse(JsonSerializer.Serialize(new Dictionary<string, string>
+            {
+                { "version", "1.0.1" }
+            }));
 
-            result = evaluator.Apply(rule, data);
-            Assert.True(result.IsTruthy());
+            result = JsonLogic.Apply(rule, data);
+            Assert.True(result.GetValue<bool>());
         }
 
         [Fact]
         public void EvaluateVersionLess()
         {
             // Arrange
-            var evaluator = new JsonLogicEvaluator(EvaluateOperators.Default);
-            var semVerEvaluator = new SemVerEvaluator();
-            EvaluateOperators.Default.AddOperator("sem_ver", semVerEvaluator.Evaluate);
+            RuleRegistry.AddRule("sem_ver", new SemVerRule());
 
             var targetingString = @"{""sem_ver"": [
-              {
-                ""var"": [
-                  ""version""
-                ]
-              },
+              { ""var"": ""version"" },
               ""<"",
               ""1.0.2""
             ]}";
 
-            // Parse json into hierarchical structure
-            var rule = JObject.Parse(targetingString);
+            var rule = JsonNode.Parse(targetingString);
 
-            var data = new Dictionary<string, string> { { "version", "1.0.1" } };
+            var data = JsonNode.Parse(JsonSerializer.Serialize(new Dictionary<string, string>
+            {
+                { "version", "1.0.1" }
+            }));
 
             // Act & Assert
-            var result = evaluator.Apply(rule, data);
-            Assert.True(result.IsTruthy());
+            var result = JsonLogic.Apply(rule, data);
+            Assert.True(result.GetValue<bool>());
 
-            data.Clear();
-            data.Add("version", "1.0.2");
+            data = JsonNode.Parse(JsonSerializer.Serialize(new Dictionary<string, string>
+            {
+                { "version", "1.0.2" }
+            }));
 
-            result = evaluator.Apply(rule, data);
-            Assert.False(result.IsTruthy());
+            result = JsonLogic.Apply(rule, data);
+            Assert.False(result.GetValue<bool>());
         }
 
         [Fact]
         public void EvaluateVersionLessOrEqual()
         {
             // Arrange
-            var evaluator = new JsonLogicEvaluator(EvaluateOperators.Default);
-            var semVerEvaluator = new SemVerEvaluator();
-            EvaluateOperators.Default.AddOperator("sem_ver", semVerEvaluator.Evaluate);
+            RuleRegistry.AddRule("sem_ver", new SemVerRule());
 
             var targetingString = @"{""sem_ver"": [
-              {
-                ""var"": [
-                  ""version""
-                ]
-              },
+              { ""var"": ""version"" },
               ""<="",
               ""1.0.2""
             ]}";
 
-            // Parse json into hierarchical structure
-            var rule = JObject.Parse(targetingString);
+            var rule = JsonNode.Parse(targetingString);
 
-            var data = new Dictionary<string, string> { { "version", "1.0.1" } };
+            var data = JsonNode.Parse(JsonSerializer.Serialize(new Dictionary<string, string>
+            {
+                { "version", "1.0.1" }
+            }));
 
             // Act & Assert
-            var result = evaluator.Apply(rule, data);
-            Assert.True(result.IsTruthy());
+            var result = JsonLogic.Apply(rule, data);
+            Assert.True(result.GetValue<bool>());
 
-            data.Clear();
-            data.Add("version", "1.0.2");
+            data = JsonNode.Parse(JsonSerializer.Serialize(new Dictionary<string, string>
+            {
+                { "version", "1.0.2" }
+            }));
 
-            result = evaluator.Apply(rule, data);
-            Assert.True(result.IsTruthy());
+            result = JsonLogic.Apply(rule, data);
+            Assert.True(result.GetValue<bool>());
 
-            data.Clear();
-            data.Add("version", "1.0.3");
+            data = JsonNode.Parse(JsonSerializer.Serialize(new Dictionary<string, string>
+            {
+                { "version", "1.0.3" }
+            }));
 
-            result = evaluator.Apply(rule, data);
-            Assert.False(result.IsTruthy());
+            result = JsonLogic.Apply(rule, data);
+            Assert.False(result.GetValue<bool>());
         }
 
         [Fact]
         public void EvaluateVersionGreater()
         {
             // Arrange
-            var evaluator = new JsonLogicEvaluator(EvaluateOperators.Default);
-            var semVerEvaluator = new SemVerEvaluator();
-            EvaluateOperators.Default.AddOperator("sem_ver", semVerEvaluator.Evaluate);
+            RuleRegistry.AddRule("sem_ver", new SemVerRule());
 
             var targetingString = @"{""sem_ver"": [
-              {
-                ""var"": [
-                  ""version""
-                ]
-              },
+              { ""var"": ""version"" },
               "">"",
               ""1.0.2""
             ]}";
 
-            // Parse json into hierarchical structure
-            var rule = JObject.Parse(targetingString);
+            var rule = JsonNode.Parse(targetingString);
 
-            var data = new Dictionary<string, string> { { "version", "1.0.3" } };
+            var data = JsonNode.Parse(JsonSerializer.Serialize(new Dictionary<string, string>
+            {
+                { "version", "1.0.3" }
+            }));
 
             // Act & Assert
-            var result = evaluator.Apply(rule, data);
-            Assert.True(result.IsTruthy());
+            var result = JsonLogic.Apply(rule, data);
+            Assert.True(result.GetValue<bool>());
 
-            data.Clear();
-            data.Add("version", "1.0.2");
+            data = JsonNode.Parse(JsonSerializer.Serialize(new Dictionary<string, string>
+            {
+                { "version", "1.0.2" }
+            }));
 
-            result = evaluator.Apply(rule, data);
-            Assert.False(result.IsTruthy());
+            result = JsonLogic.Apply(rule, data);
+            Assert.False(result.GetValue<bool>());
         }
 
         [Fact]
         public void EvaluateVersionGreaterOrEqual()
         {
             // Arrange
-            var evaluator = new JsonLogicEvaluator(EvaluateOperators.Default);
-            var semVerEvaluator = new SemVerEvaluator();
-            EvaluateOperators.Default.AddOperator("sem_ver", semVerEvaluator.Evaluate);
+            RuleRegistry.AddRule("sem_ver", new SemVerRule());
 
             var targetingString = @"{""sem_ver"": [
-                      {
-                        ""var"": [
-                          ""version""
-                        ]
-                      },
+                      { ""var"": ""version"" },
                       "">="",
                       ""1.0.2""
                     ]}";
 
-            // Parse json into hierarchical structure
-            var rule = JObject.Parse(targetingString);
+            var rule = JsonNode.Parse(targetingString);
 
-            var data = new Dictionary<string, string> { { "version", "1.0.2" } };
+            var data = JsonNode.Parse(JsonSerializer.Serialize(new Dictionary<string, string>
+            {
+                { "version", "1.0.2" }
+            }));
 
             // Act & Assert
-            var result = evaluator.Apply(rule, data);
-            Assert.True(result.IsTruthy());
+            var result = JsonLogic.Apply(rule, data);
+            Assert.True(result.GetValue<bool>());
 
-            data.Clear();
-            data.Add("version", "1.0.3");
+            data = JsonNode.Parse(JsonSerializer.Serialize(new Dictionary<string, string>
+            {
+                { "version", "1.0.3" }
+            }));
 
-            result = evaluator.Apply(rule, data);
-            Assert.True(result.IsTruthy());
+            result = JsonLogic.Apply(rule, data);
+            Assert.True(result.GetValue<bool>());
 
-            data.Clear();
-            data.Add("version", "1.0.1");
+            data = JsonNode.Parse(JsonSerializer.Serialize(new Dictionary<string, string>
+            {
+                { "version", "1.0.1" }
+            }));
 
-            result = evaluator.Apply(rule, data);
-            Assert.False(result.IsTruthy());
+            result = JsonLogic.Apply(rule, data);
+            Assert.False(result.GetValue<bool>());
         }
 
         [Fact]
         public void EvaluateVersionMatchMajor()
         {
             // Arrange
-            var evaluator = new JsonLogicEvaluator(EvaluateOperators.Default);
-            var semVerEvaluator = new SemVerEvaluator();
-            EvaluateOperators.Default.AddOperator("sem_ver", semVerEvaluator.Evaluate);
+            RuleRegistry.AddRule("sem_ver", new SemVerRule());
 
             var targetingString = @"{""sem_ver"": [
-              {
-                ""var"": [
-                  ""version""
-                ]
-              },
+              { ""var"": ""version"" },
               ""^"",
               ""1.0.0""
             ]}";
 
-            // Parse json into hierarchical structure
-            var rule = JObject.Parse(targetingString);
+            var rule = JsonNode.Parse(targetingString);
 
-            var data = new Dictionary<string, string> { { "version", "1.0.3" } };
+            var data = JsonNode.Parse(JsonSerializer.Serialize(new Dictionary<string, string>
+            {
+                { "version", "1.0.3" }
+            }));
 
             // Act & Assert
-            var result = evaluator.Apply(rule, data);
-            Assert.True(result.IsTruthy());
+            var result = JsonLogic.Apply(rule, data);
+            Assert.True(result.GetValue<bool>());
 
-            data.Clear();
-            data.Add("version", "2.0.0");
+            data = JsonNode.Parse(JsonSerializer.Serialize(new Dictionary<string, string>
+            {
+                { "version", "2.0.0" }
+            }));
 
-            result = evaluator.Apply(rule, data);
-            Assert.False(result.IsTruthy());
+            result = JsonLogic.Apply(rule, data);
+            Assert.False(result.GetValue<bool>());
         }
 
         [Fact]
         public void EvaluateVersionMatchMinor()
         {
             // Arrange
-            var evaluator = new JsonLogicEvaluator(EvaluateOperators.Default);
-            var semVerEvaluator = new SemVerEvaluator();
-            EvaluateOperators.Default.AddOperator("sem_ver", semVerEvaluator.Evaluate);
+            RuleRegistry.AddRule("sem_ver", new SemVerRule());
 
             var targetingString = @"{""sem_ver"": [
-              {
-                ""var"": [
-                  ""version""
-                ]
-              },
+              { ""var"": ""version"" },
               ""~"",
               ""1.3.0""
             ]}";
 
-            // Parse json into hierarchical structure
-            var rule = JObject.Parse(targetingString);
+            var rule = JsonNode.Parse(targetingString);
 
-            var data = new Dictionary<string, string> { { "version", "1.3.3" } };
+            var data = JsonNode.Parse(JsonSerializer.Serialize(new Dictionary<string, string>
+            {
+                { "version", "1.3.3" }
+            }));
 
             // Act & Assert
-            var result = evaluator.Apply(rule, data);
-            Assert.True(result.IsTruthy());
+            var result = JsonLogic.Apply(rule, data);
+            Assert.True(result.GetValue<bool>());
 
-            data.Clear();
-            data.Add("version", "2.3.0");
+            data = JsonNode.Parse(JsonSerializer.Serialize(new Dictionary<string, string>
+            {
+                { "version", "2.3.0" }
+            }));
 
-            result = evaluator.Apply(rule, data);
-            Assert.False(result.IsTruthy());
+            result = JsonLogic.Apply(rule, data);
+            Assert.False(result.GetValue<bool>());
         }
 
         [Fact]
         public void EvaluateVersionTooFewArguments()
         {
             // Arrange
-            var evaluator = new JsonLogicEvaluator(EvaluateOperators.Default);
-            var semVerEvaluator = new SemVerEvaluator();
-            EvaluateOperators.Default.AddOperator("sem_ver", semVerEvaluator.Evaluate);
+            RuleRegistry.AddRule("sem_ver", new SemVerRule());
 
             var targetingString = @"{""sem_ver"": [
-              {
-                ""var"": [
-                  ""version""
-                ]
-              },
+              { ""var"": ""version"" },
               ""~""
             ]}";
 
-            // Parse json into hierarchical structure
-            var rule = JObject.Parse(targetingString);
+            var rule = JsonNode.Parse(targetingString);
 
-            var data = new Dictionary<string, string> { { "version", "1.3.3" } };
+            var data = JsonNode.Parse(JsonSerializer.Serialize(new Dictionary<string, string>
+            {
+                { "version", "1.3.3" }
+            }));
 
             // Act & Assert
-            var result = evaluator.Apply(rule, data);
-            Assert.False(result.IsTruthy());
+            var result = JsonLogic.Apply(rule, data);
+            Assert.False(result.GetValue<bool>());
         }
 
         [Fact]
         public void EvaluateVersionNotAValidVersion()
         {
             // Arrange
-            var evaluator = new JsonLogicEvaluator(EvaluateOperators.Default);
-            var semVerEvaluator = new SemVerEvaluator();
-            EvaluateOperators.Default.AddOperator("sem_ver", semVerEvaluator.Evaluate);
+            RuleRegistry.AddRule("sem_ver", new SemVerRule());
 
             var targetingString = @"{""sem_ver"": [
-              {
-                ""var"": [
-                  ""version""
-                ]
-              },
+              { ""var"": ""version"" },
               ""~"",
               ""test""
             ]}";
 
-            // Parse json into hierarchical structure
-            var rule = JObject.Parse(targetingString);
+            var rule = JsonNode.Parse(targetingString);
 
-            var data = new Dictionary<string, string> { { "version", "1.3.3" } };
+            var data = JsonNode.Parse(JsonSerializer.Serialize(new Dictionary<string, string>
+            {
+                { "version", "1.3.3" }
+            }));
 
             // Act & Assert
-            var result = evaluator.Apply(rule, data);
-            Assert.False(result.IsTruthy());
+            var result = JsonLogic.Apply(rule, data);
+            Assert.False(result.GetValue<bool>());
         }
     }
 }
