702128e4588ac2ac1acaad862199754dbcc23b52
André Silva <2493377+askpt@users.noreply.github.com>
2025-05-02 08:03:06 +0100

    Refactor converters and exception handling in GOFeatureFlag provider
    - Updated DictionaryConverter to improve code clarity and structure.
    - Enhanced JsonConverterExtensions with better documentation.
    - Refined OpenFeatureStructureConverter and OpenFeatureValueConverter for consistency and readability.
    - Simplified exception classes (FlagDisabled, FlagNotFoundError, GeneralError, etc.) by removing unnecessary comments and improving documentation.
    - Improved error handling in GoFeatureFlagException and its derived classes.
    - Streamlined the implementation of GoFeatureFlagExtensions for better usability.
    - Enhanced EnrichEvaluationContextHook to clarify its purpose and improve metadata handling.
    - Refactored ExporterMetadata to encapsulate metadata building logic more effectively.
    - Improved the structure and clarity of OfrepRequest and OfrepResponse classes.
    
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- src/OpenFeature.Contrib.Providers.GOFeatureFlag/exception/GeneralError.cs
+++ src/OpenFeature.Contrib.Providers.GOFeatureFlag/exception/GeneralError.cs
@@ -2,21 +2,20 @@ using System;
 using OpenFeature.Constant;
 using OpenFeature.Error;
 
-namespace OpenFeature.Contrib.Providers.GOFeatureFlag.exception
+namespace OpenFeature.Contrib.Providers.GOFeatureFlag.exception;
+
+/// <summary>
+///     Exception throw when we don't have a specific case.
+/// </summary>
+public class GeneralError : FeatureProviderException
 {
     /// <summary>
-    ///     Exception throw when we don't have a specific case.
+    ///     Constructor of the exception
     /// </summary>
-    public class GeneralError : FeatureProviderException
+    /// <param name="message">Message to display</param>
+    /// <param name="innerException">Original exception</param>
+    public GeneralError(string message, Exception innerException = null) : base(ErrorType.General, message,
+        innerException)
     {
-        /// <summary>
-        ///     Constructor of the exception
-        /// </summary>
-        /// <param name="message">Message to display</param>
-        /// <param name="innerException">Original exception</param>
-        public GeneralError(string message, Exception innerException = null) : base(ErrorType.General, message,
-            innerException)
-        {
-        }
     }
 }
--- src/OpenFeature.Contrib.Providers.GOFeatureFlag/exception/TypeMismatchError.cs
+++ src/OpenFeature.Contrib.Providers.GOFeatureFlag/exception/TypeMismatchError.cs
@@ -2,21 +2,20 @@ using System;
 using OpenFeature.Constant;
 using OpenFeature.Error;
 
-namespace OpenFeature.Contrib.Providers.GOFeatureFlag.exception
+namespace OpenFeature.Contrib.Providers.GOFeatureFlag.exception;
+
+/// <summary>
+///     Exception throw when the type we received from GO Feature Flag is different than the one expected.
+/// </summary>
+public class TypeMismatchError : FeatureProviderException
 {
     /// <summary>
-    ///     Exception throw when the type we received from GO Feature Flag is different than the one expected.
+    ///     Constructor of the exception
     /// </summary>
-    public class TypeMismatchError : FeatureProviderException
+    /// <param name="message">Message to display</param>
+    /// <param name="innerException">Original exception</param>
+    public TypeMismatchError(string message, Exception innerException = null) : base(ErrorType.TypeMismatch,
+        message, innerException)
     {
-        /// <summary>
-        ///     Constructor of the exception
-        /// </summary>
-        /// <param name="message">Message to display</param>
-        /// <param name="innerException">Original exception</param>
-        public TypeMismatchError(string message, Exception innerException = null) : base(ErrorType.TypeMismatch,
-            message, innerException)
-        {
-        }
     }
 }
