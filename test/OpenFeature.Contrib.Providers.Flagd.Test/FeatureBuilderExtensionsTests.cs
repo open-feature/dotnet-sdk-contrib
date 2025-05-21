1f945163603f3b44eff5de4fe7a52dcd3d09a914
Kyle Julian <38759683+kylejuliandev@users.noreply.github.com>
2025-05-21 18:57:25 +0100

    Add unit tests to cover off Flagd DI extension methods
    Signed-off-by: Kyle Julian <38759683+kylejuliandev@users.noreply.github.com>

--- test/OpenFeature.Contrib.Providers.Flagd.Test/FeatureBuilderExtensionsTests.cs
+++ test/OpenFeature.Contrib.Providers.Flagd.Test/FeatureBuilderExtensionsTests.cs
@@ -1,4 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using OpenFeature.DependencyInjection.Providers.Flagd;
 using Xunit;
 
@@ -25,6 +27,43 @@ public class FeatureBuilderExtensionsTests
         var provider = services.GetService<FeatureProvider>();
         Assert.NotNull(provider);
         Assert.Equal("flagd Provider", provider.GetMetadata().Name);
+        Assert.IsType<FlagdProvider>(provider);
+
+        var flagdProvider = (FlagdProvider)provider;
+        var config = flagdProvider.GetConfig();
+        Assert.NotNull(config);
+        Assert.Multiple(
+            () => Assert.Equal("localhost", config.Host),
+            () => Assert.Equal(8013, config.Port),
+            () => Assert.False(config.UseTls, "UseTls is disabled by default"),
+            () => Assert.False(config.CacheEnabled, "CacheEnabled is disabled by default"),
+            () => Assert.Equal(10, config.MaxCacheSize),
+            () => Assert.Equal(string.Empty, config.CertificatePath),
+            () => Assert.Equal(string.Empty, config.SocketPath),
+            () => Assert.Equal(3, config.MaxEventStreamRetries),
+            () => Assert.Equal(ResolverType.RPC, config.ResolverType),
+            () => Assert.Equal(string.Empty, config.SourceSelector)
+        );
+    }
+
+    [Fact]
+    public void AddFlagdProvider_WithDomain_And_NoConfiguration_ShouldReturnOpenFeatureBuilder()
+    {
+        // Arrange
+        using var services = new ServiceCollection()
+            .AddOpenFeature(builder =>
+            {
+                // Act
+                builder.AddFlagdProvider("test-domain");
+            })
+            .BuildServiceProvider();
+
+        // Assert
+        var client = services.GetService<IFeatureClient>();
+        Assert.NotNull(client);
+
+        var provider = services.GetKeyedService<FeatureProvider>("test-domain");
+        Assert.NotNull(provider);
     }
 
     [Fact]
@@ -37,7 +76,16 @@ public class FeatureBuilderExtensionsTests
                 // Act
                 builder.AddFlagdProvider(new FlagdProviderOptions
                 {
-                    Host = "localhost"
+                    Host = "flagdtest",
+                    Port = 1234,
+                    UseTls = true,
+                    CacheEnabled = true,
+                    MaxCacheSize = 500,
+                    CertificatePath = "/path/to/cert",
+                    SocketPath = "/path/to/socket",
+                    MaxEventStreamRetries = -1,
+                    ResolverType = ResolverType.IN_PROCESS,
+                    SourceSelector = "source-selector"
                 });
             })
             .BuildServiceProvider();
@@ -49,27 +97,93 @@ public class FeatureBuilderExtensionsTests
         var provider = services.GetService<FeatureProvider>();
         Assert.NotNull(provider);
         Assert.Equal("flagd Provider", provider.GetMetadata().Name);
+        Assert.IsType<FlagdProvider>(provider);
+
+        var flagdProvider = (FlagdProvider)provider;
+        var config = flagdProvider.GetConfig();
+        Assert.NotNull(config);
+        Assert.Multiple(
+            () => Assert.Equal("flagdtest", config.Host),
+            () => Assert.Equal(1234, config.Port),
+            () => Assert.True(config.UseTls),
+            () => Assert.True(config.CacheEnabled),
+            () => Assert.Equal(500, config.MaxCacheSize),
+            () => Assert.Equal("/path/to/cert", config.CertificatePath),
+            () => Assert.Equal("/path/to/socket", config.SocketPath),
+            () => Assert.Equal(-1, config.MaxEventStreamRetries),
+            () => Assert.Equal(ResolverType.IN_PROCESS, config.ResolverType),
+            () => Assert.Equal("source-selector", config.SourceSelector)
+        );
     }
 
     [Fact]
-    public void AddFlagdProvider_WithConfigBuilderDelegate_ShouldReturnOpenFeatureBuilder()
+    public void AddFlagdProvider_WithDomain_ShouldReturnOpenFeatureBuilder()
     {
         // Arrange
         using var services = new ServiceCollection()
             .AddOpenFeature(builder =>
             {
                 // Act
-                builder.AddFlagdProvider(config => config
-                    .WithHost("localhost"));
+                builder.AddFlagdProvider("test-domain", new FlagdProviderOptions
+                {
+                    Host = "flagdtest",
+                    Port = 1234,
+                    UseTls = true,
+                    CacheEnabled = true,
+                    MaxCacheSize = 500,
+                    CertificatePath = "/path/to/cert",
+                    SocketPath = "/path/to/socket",
+                    MaxEventStreamRetries = -1,
+                    ResolverType = ResolverType.IN_PROCESS,
+                    SourceSelector = "source-selector"
+                });
             })
             .BuildServiceProvider();
 
         // Assert
-        var client = services.GetService<IFeatureClient>();
-        Assert.NotNull(client);
-
-        var provider = services.GetService<FeatureProvider>();
+        var provider = services.GetKeyedService<FeatureProvider>("test-domain");
         Assert.NotNull(provider);
-        Assert.Equal("flagd Provider", provider.GetMetadata().Name);
+    }
+
+    [Fact]
+    public void AddFlagdProvider_WithNoLoggingServicesRegistered_AddsNullLoggerToConfig()
+    {
+        // Arrange
+        using var services = new ServiceCollection()
+            .AddOpenFeature(builder =>
+            {
+                // Act
+                builder.AddFlagdProvider();
+            })
+            .BuildServiceProvider();
+
+        // Assert
+        var config = GetFlagdProviderConfig(services);
+        Assert.IsType<NullLogger<FlagdProvider>>(config.Logger);
+    }
+
+    [Fact]
+    public void AddFlagdProvider_WithLoggingServicesRegistered_AddsFlagdLoggerToConfig()
+    {
+        // Arrange
+        using var services = new ServiceCollection()
+            .AddLogging()
+            .AddOpenFeature(builder =>
+            {
+                // Act
+                builder.AddFlagdProvider();
+            })
+            .BuildServiceProvider();
+
+        // Assert
+        var config = GetFlagdProviderConfig(services);
+        Assert.IsType<ILogger<FlagdProvider>>(config.Logger, exactMatch: false);
+    }
+
+    private static FlagdConfig GetFlagdProviderConfig(ServiceProvider services)
+    {
+        var provider = services.GetService<FeatureProvider>();
+        var flagdProvider = (FlagdProvider)provider;
+        return flagdProvider.GetConfig();
     }
 }
