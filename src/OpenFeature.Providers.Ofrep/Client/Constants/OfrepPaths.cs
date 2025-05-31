f23c0050a8787f9563ab2dd72effe5290ee0c640
André Silva <2493377+askpt@users.noreply.github.com>
2025-05-31 17:00:54 +0100

    refactor: Introduce OfrepPaths class to centralize API path constants in OfrepClient
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

---
+++ src/OpenFeature.Providers.Ofrep/Client/Constants/OfrepPaths.cs
@@ -0,0 +1,9 @@
+namespace OpenFeature.Providers.Ofrep.Client.Constants;
+
+internal static class OfrepPaths
+{
+    // OFREP API paths
+    internal const string Evaluate = "/ofrep/v1/evaluate/flags/";
+    internal const string BulkEvaluate = "/ofrep/v1/evaluate/flags";
+    internal const string Configuration = "/ofrep/v1/configuration";
+}
