5574ee6a1625173ffed4d09f046be2a1d8e11a39
André Silva <2493377+askpt@users.noreply.github.com>
2025-06-02 15:49:22 +0100

    refactor: Remove cache-related properties from ProviderCapabilities
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- src/OpenFeature.Providers.Ofrep/Models/ProviderCapabilities.cs
+++ src/OpenFeature.Providers.Ofrep/Models/ProviderCapabilities.cs
@@ -8,20 +8,8 @@ namespace OpenFeature.Providers.Ofrep.Models;
 public class ProviderCapabilities
 {
     /// <summary>
-    /// Gets or sets the cache invalidation capabilities.
-    /// </summary>
-    [JsonPropertyName("cacheInvalidation")]
-    public ProviderCacheInvalidation? CacheInvalidation { get; set; }
-
-    /// <summary>
     /// Gets or sets the flag evaluation capabilities.
     /// </summary>
     [JsonPropertyName("flagEvaluation")]
     public ProviderFlagEvaluation? FlagEvaluation { get; set; }
-
-    /// <summary>
-    /// Gets or sets the caching capabilities.
-    /// </summary>
-    [JsonPropertyName("caching")]
-    public ProviderCaching? Caching { get; set; }
 }
