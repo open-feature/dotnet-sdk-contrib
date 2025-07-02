0cd2e34331f70c18e03b23a97ce6b79f9c4a982b
André Silva <2493377+askpt@users.noreply.github.com>
2025-07-02 20:35:56 +0100

    Update IsExternalInit.cs comments for clarity on usage and compatibility
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- src/OpenFeature.Providers.GOFeatureFlag/Helpers/IsExternalInit.cs
+++ src/OpenFeature.Providers.GOFeatureFlag/Helpers/IsExternalInit.cs
@@ -1,6 +1,10 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+// This file is a placeholder for the IsExternalInit class, which is used to support C# 9.0 init-only properties
+// in projects targeting .NET Standard 2.0 and earlier. This file should not be included in projects targeting
+// .NET 5.0 or later, as the IsExternalInit class is provided by the runtime in those versions.
+#if !NET5_0_OR_GREATER
 namespace System.Runtime.CompilerServices;
 
 /// <summary>
@@ -10,3 +14,4 @@ namespace System.Runtime.CompilerServices;
 internal static class IsExternalInit
 {
 }
+#endif
