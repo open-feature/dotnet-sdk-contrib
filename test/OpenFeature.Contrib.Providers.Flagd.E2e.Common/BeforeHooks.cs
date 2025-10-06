0375e383ff98719d6d0d52e0be270efad79a8e24
Kyle Julian <38759683+kylejuliandev@users.noreply.github.com>
2025-10-06 17:53:46 +0100

    Add back ability to skip E2E tests
    Signed-off-by: Kyle Julian <38759683+kylejuliandev@users.noreply.github.com>

--- test/OpenFeature.Contrib.Providers.Flagd.E2e.Common/BeforeHooks.cs
+++ test/OpenFeature.Contrib.Providers.Flagd.E2e.Common/BeforeHooks.cs
@@ -1,11 +1,21 @@
+using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Reqnroll;
+using Xunit;
 
 namespace OpenFeature.Contrib.Providers.Flagd.E2e.Common;
 
 [Binding]
 public class BeforeHooks
 {
+    private readonly IConfiguration _configuration;
+
+    public BeforeHooks(IConfiguration configuration)
+    {
+        this._configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+    }
+
     [BeforeTestRun]
     public static async Task BeforeTestRunAsync(FlagdTestBedContainer container)
     {
@@ -16,5 +26,11 @@ public class BeforeHooks
     public static async Task AfterTestRunAsync(FlagdTestBedContainer container)
     {
         await container.Container.StopAsync().ConfigureAwait(false);
+    }
+
+    [BeforeScenario]
+    public void BeforeScenario()
+    {
+        Skip.If(this._configuration["E2E"] != "true", "Skipping test as E2E tests are disabled, enable them by updating the appsettings.json.");
     }
 }
