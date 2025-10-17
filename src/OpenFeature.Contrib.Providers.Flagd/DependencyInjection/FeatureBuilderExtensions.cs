2daa3491b89d48435e4d630a85c1426bdc298952
André Silva <2493377+askpt@users.noreply.github.com>
2025-10-17 07:04:36 +0100

    fix: Correct namespace order in FeatureBuilderExtensions.cs
    Signed-off-by: André Silva <2493377+askpt@users.noreply.github.com>

--- src/OpenFeature.Contrib.Providers.Flagd/DependencyInjection/FeatureBuilderExtensions.cs
+++ src/OpenFeature.Contrib.Providers.Flagd/DependencyInjection/FeatureBuilderExtensions.cs
@@ -3,9 +3,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Options;
-using OpenFeature.Hosting;
 using OpenFeature.Contrib.Providers.Flagd;
 using OpenFeature.Contrib.Providers.Flagd.DependencyInjection;
+using OpenFeature.Hosting;
 
 namespace OpenFeature.DependencyInjection.Providers.Flagd;
 
