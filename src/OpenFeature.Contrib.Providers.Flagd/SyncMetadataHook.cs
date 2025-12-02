c2f5766693f2f4283e1a64ad85282f87edded84d
Kyle Julian <38759683+kylejuliandev@users.noreply.github.com>
2025-12-02 23:53:03 +0000

    Add XML comments
    Signed-off-by: Kyle Julian <38759683+kylejuliandev@users.noreply.github.com>

--- src/OpenFeature.Contrib.Providers.Flagd/SyncMetadataHook.cs
+++ src/OpenFeature.Contrib.Providers.Flagd/SyncMetadataHook.cs
@@ -6,15 +6,23 @@ using OpenFeature.Model;
 
 namespace OpenFeature.Contrib.Providers.Flagd;
 
+/// <summary>
+/// Provides a synchronous hook that supplies metadata using a specified evaluation context callback.
+/// </summary>
 internal class SyncMetadataHook : Hook
 {
     private readonly Func<EvaluationContext> _evaluationContextSupplier;
 
-    public SyncMetadataHook(Func<EvaluationContext> evaluationContextSupplier)
+    /// <summary>
+    /// Initializes a new instance of the SyncMetadataHook class using the specified evaluation context callback.
+    /// </summary>
+    /// <param name="evaluationContextCallback">A delegate that provides an EvaluationContext instance used by the hook. Cannot be null.</param>
+    public SyncMetadataHook(Func<EvaluationContext> evaluationContextCallback)
     {
-        this._evaluationContextSupplier = evaluationContextSupplier;
+        this._evaluationContextSupplier = evaluationContextCallback ?? throw new ArgumentNullException(nameof(evaluationContextCallback));
     }
 
+    /// <inheritdoc />
     public override ValueTask<EvaluationContext> BeforeAsync<T>(HookContext<T> context, IReadOnlyDictionary<string, object> hints = null, CancellationToken cancellationToken = default)
     {
         return new ValueTask<EvaluationContext>(this._evaluationContextSupplier());
