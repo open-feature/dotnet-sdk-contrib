42b27626fae1f24417fba8c751c8236953cd6232
Todd Baert <todd.baert@dynatrace.com>
2026-04-01 15:47:54 -0400

    fixup: update testbed and excludes
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>

--- test/OpenFeature.Contrib.Providers.Flagd.E2e.RpcTest/BeforeHooks.cs
+++ test/OpenFeature.Contrib.Providers.Flagd.E2e.RpcTest/BeforeHooks.cs
@@ -26,7 +26,6 @@ public class BeforeHooks
         var tags = new HashSet<string>(scenarioTags.Concat(featureTags));
         Skip.If(!tags.Contains("rpc"), "Skipping scenario because it does not have required tag.");
         Skip.If(tags.Contains("fractional-v1"), "Skipping legacy fractional bucketing test; v2 algorithm is implemented.");
-        Skip.If(tags.Contains("fractional-v2"), "Skipping fractional-v2 test; flagd server does not support v2 bucketing yet.");
-        Skip.If(tags.Contains("fractional-nested"), "Skipping fractional-nested test; flagd server does not support nested fractional yet.");
+        Skip.If(tags.Contains("operator-errors"), "Skipping operator-errors test; flagd server does not yet fall back to default on operator errors.");
     }
 }
