79aa12901ab12c876ada130b4c3e0a2bd1a36ae5
Todd Baert <todd.baert@dynatrace.com>
2026-04-30 15:33:08 -0400

    fixup: update testbed and remove RPC excludes
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>

--- test/OpenFeature.Providers.Flagd.E2e.RpcTest/BeforeHooks.cs
+++ test/OpenFeature.Providers.Flagd.E2e.RpcTest/BeforeHooks.cs
@@ -26,10 +26,5 @@ public class BeforeHooks
         var tags = new HashSet<string>(scenarioTags.Concat(featureTags));
         Skip.If(!tags.Contains("rpc"), "Skipping scenario because it does not have required tag.");
         Skip.If(tags.Contains("fractional-v1"), "Skipping legacy fractional bucketing test; v2 algorithm is implemented.");
-        Skip.If(tags.Contains("operator-errors"), "Skipping operator-errors test; flagd server does not yet fall back to default on operator errors.");
-        Skip.If(tags.Contains("semver-edge-cases"), "Skipping semver-edge-cases; flagd server not updated.");
-        Skip.If(tags.Contains("evaluator-refs-whitespace"), "Skipping evaluator-refs-whitespace; flagd server not updated.");
-        Skip.If(tags.Contains("non-existent-evaluator-ref"), "Skipping non-existent-evaluator-ref; flagd server not updated.");
-        Skip.If(tags.Contains("fractional-single-entry"), "Skipping fractional-single-entry; flagd server not updated.");
     }
 }
